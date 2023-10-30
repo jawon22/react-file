c67a49e1f8dd652e64cea9802e81f51abfcb847e
jawon22 <winwkdnjs@naver.com>
2023-10-30 21:51:09 +0900

    React 과제 완료

---
+++ demo03/src/components/Exam04.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const Exam04 = ()=>{
+    const [text,setText] = useState('');
+
+    const TextChange = (e) => {
+        const newText = e.target.value;
+        // 글자 수를 1000자로 제한
+    if (newText.length <= 1000) {
+        setText(newText);
+      }
+    };
+    const isMax = text.length >=1000;
+    return(
+        <>
+            <div className="row">
+                <div className="col-md-10 offset-md-1">
+
+                    <div className="row mt-4 text-center">
+                        <div className="col">
+                            <h2>(Q) 주말에 뭐하세요?</h2>
+                        </div>
+                    </div>
+
+                    <div className="row mt-2">
+                        <div className="col-md-8 offset-md-2">
+                            <textarea className="form-control" rows={8} 
+                                placeholder="1000글자 내로 입력하세요"
+                                value={text}
+                                onChange={TextChange}/>
+
+                            <div className="text-end">
+                                <span style={{ color: isMax ? 'red' : 'black' }}>
+                                    {text.length}</span>/ 1000bytes  
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Exam04;
