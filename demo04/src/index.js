db454d9b488acdfe658b481807621c82752316ea
jawon22 <winwkdnjs@naver.com>
2023-11-02 14:36:40 +0900

    강사님꺼 추가

--- demo04/src/index.js
+++ demo04/src/index.js
@@ -5,12 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootswatch/dist/sandstone/bootstrap.min.css'
 // import './index.css';
 import App from './App'; 
+import App1 from './App1';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <App1/>
   </React.StrictMode>
 );
 
