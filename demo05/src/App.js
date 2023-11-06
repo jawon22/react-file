64c7137a506a98f4e9a35c976963150f32b1bc05
jawon22 <winwkdnjs@naver.com>
2023-11-06 14:21:01 +0900

    메뉴 디자인 변경 완료

--- demo05/src/App.js
+++ demo05/src/App.js
@@ -3,26 +3,23 @@ import Jumbotron from './components/Jumbotron';
 import Pocketmon from './components/Pocketmon';
 import Book from './components/Book';
 import Home from './components/Home';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className='container-fluid my-5'>
       <div className='row'>
         <div className='col-md-8 offset-md-2'>
-          <Jumbotron title="demo05 생성" content="생성했지렁"/>
+          {/* <Jumbotron title="demo05 생성" content="생성했지렁"/> */}
           {/* 상단 메뉴 영역 */}
-            <div>
-              <NavLink to={"/"} className="me-2">홈</NavLink>
-              <NavLink to={"/pocketmon"} className="me-2">포켓몬스터</NavLink>
-              <NavLink to={"/book"}>도서정보</NavLink>
-            </div>
+          <Menu/>
 
           {/* 본문 영역 */}
             <div>
               <Routes>
                 <Route exact path="/" element={<Home/>}></Route> 
                 <Route path="/pocketmon" element={<Pocketmon/>}></Route>
-                <Route path="book" element={<Book/>}></Route>
+                <Route path="/book" element={<Book/>}></Route>
               </Routes>
             </div>
 
---
+++ demo05/src/components/Menu.js
@@ -0,0 +1,35 @@
+import { NavLink, useLocation } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.js'
+
+const Menu = (props) => {
+    const location = useLocation();
+    // console.log(location);
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <NavLink to={"/"} className="me-2 navbar-brand">제목</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarColor02">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+
+                                <NavLink className={`nav-link ${location.pathname === '/pocketmon' ? 'active' :''}`} to="/pocketmon">포켓몬
+                                    <span className="visually-hidden">(current)</span>
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className={`nav-link ${location.pathname === '/book' ? 'active' :''}`} to="/book">도서</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Menu;
